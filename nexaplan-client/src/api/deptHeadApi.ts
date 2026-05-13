392c6e2dbd9b1226fac69b60c77da08d989171f1
tao <tjohnracil@gmail.com>
2026-05-13 20:53:46 +0800

    feat: implement API layer for administrative roles and integrate core services in client application

--- nexaplan-client/src/api/deptHeadApi.ts
+++ nexaplan-client/src/api/deptHeadApi.ts
@@ -1,4 +1,4 @@
-const API_BASE = 'http://nexaplan.runasp.net/api/dept-head';
+const API_BASE = 'https://nexaplan.runasp.net/api/dept-head';
 
 function getHeaders() {
   const userStr = localStorage.getItem('user');
